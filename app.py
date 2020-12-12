ae002cc0f5e3e2d027ae14a4cea95ebf731f9a97
Naykoh <37302578+Naykoh@users.noreply.github.com>
2020-12-12 19:38:14 +0100

    model working, trying to print result

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, request, render_template
-#from src.predict import prediction
+from src.recommendations import recommendations
 
 app = Flask(__name__)
 
+def tweets(sentence):
+	tweets_block = ""
+	if sentence:
+		res= recommendations(sentence)
+		for index in res:
+			#tweets_block += "<tr id=" + str(index) + " > <td>" + str(index) + "</td> <td>" + res[index] + "</td> </tr>"
+			print(index)
+
+
+	print(tweets_block)	
+	return render_template('index.html', tweets=tweets_block)
 
 @app.route('/', methods=['GET','POST'])
 def index():
 	if request.method == 'POST':
 		output = request.form 
-		return ('oui')
+		return tweets(output['sentence'])
 
 	return render_template('index.html')
 
---
+++ src/recommendations.py
@@ -0,0 +1,106 @@
+import pickle
+import pandas as pd
+import numpy as np
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import RegexpTokenizer
+import re
+import string
+import random
+from PIL import Image
+import requests
+from io import BytesIO
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
+from gensim.models import Word2Vec
+from gensim.models.phrases import Phrases, Phraser
+from matplotlib import pyplot
+from gensim.models import KeyedVectors
+
+from pathlib import Path
+
+
+
+with open('../models/google_model.pickle', 'rb') as data:
+    google_model = pickle.load(data)
+
+
+def _removeNonAscii(s):
+    return "".join(i for i in s if  ord(i)<128)
+
+def make_lower_case(text):
+    return text.lower()
+
+def remove_stop_words(text):
+    text = text.split()
+    stops = set(stopwords.words("english"))
+    text = [w for w in text if not w in stops]
+    text = " ".join(text)
+    return text
+
+def remove_html(text):
+    html_pattern = re.compile('<.*?>')
+    return html_pattern.sub(r'', text)
+
+def remove_punctuation(text):
+    tokenizer = RegexpTokenizer(r'\w+')
+    text = tokenizer.tokenize(text)
+    text = " ".join(text)
+    return text
+
+
+
+df = pd.read_csv("../data/tweets_1.csv")
+
+df['text'] = df['text'].astype(str)
+df['cleaned'] = df['text'].apply(_removeNonAscii)
+
+df['cleaned'] = df.cleaned.apply(func = make_lower_case)
+df['cleaned'] = df.cleaned.apply(func = remove_stop_words)
+df['cleaned'] = df.cleaned.apply(func=remove_punctuation)
+df['cleaned'] = df.cleaned.apply(func=remove_html)
+
+
+# genere average word2dec pour chaque champ
+def vectors(x):
+    
+    
+    global word_embeddings
+    word_embeddings = []
+
+    
+    for line in df['cleaned']:
+        avgword2vec = None
+        count = 0
+        for word in line.split():
+            if word in google_model.wv.vocab:
+                count += 1
+                if avgword2vec is None:
+                    avgword2vec = google_model[word]
+                else:
+                    avgword2vec = avgword2vec + google_model[word]
+                
+        if avgword2vec is not None:
+            avgword2vec = avgword2vec / count
+        
+            word_embeddings.append(avgword2vec)
+
+
+
+def recommendations(text):
+    vectors(df)
+    cosine_similarities = cosine_similarity(word_embeddings, word_embeddings)
+    books = df[['text', 'id']]
+    indices = pd.Series(df.index, index = df['text']).drop_duplicates()  
+    idx=df[df['text']==text].index.tolist()[0]
+    sim_scores = list(enumerate(cosine_similarities[idx]))
+    sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
+    sim_scores = sim_scores[1:20]
+    book_indices = [i[0] for i in sim_scores]
+    recommend = books.iloc[book_indices]
+    #print(sim_scores)
+    return(recommend)
